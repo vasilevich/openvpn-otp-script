38ef8d8d27e8fe032298ebb4cec10562049a2c05
yosef langer <yosef@doar.systems>
2023-02-21 14:02:47 +0200

    Create create-user.sh
    creates an user

---
+++ create-user.sh
@@ -0,0 +1,12 @@
+USER=1$
+
+
+docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full $USER nopass
+
+docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_otp_user $USER
+
+docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient $USER > $USER.ovpn
+
+
+
+google-authenticator --time-based --disallow-reuse --force --rate-limit=3 --rate-time=30 --window-size=3 \ -l "${1}@${OVPN_CN}" -s /etc/openvpn/otp/${1}.google_authenticator
