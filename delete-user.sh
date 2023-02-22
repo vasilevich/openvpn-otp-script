88cf76deb8dc138aaa963881343ddf088bbd8ff0
yosef langer <yosef@doar.systems>
2023-02-22 14:48:19 +0200

    Update delete-user.sh

--- delete-user.sh
+++ delete-user.sh
@@ -3,3 +3,8 @@ USER=$1
 
 
 docker run --rm -it -v $OVPN_DATA:/etc/openvpn kylemanna/openvpn ovpn_revokeclient $USER
+
+# make sure no leftovers
+docker run --rm -it -v $OVPN_DATA:/etc/openvpn kylemanna/openvpn rm /etc/openvpn/pki/reqs/$USER.req
+docker run --rm -it -v $OVPN_DATA:/etc/openvpn kylemanna/openvpn rm /etc/openvpn/pki/private/$USER.key
+
