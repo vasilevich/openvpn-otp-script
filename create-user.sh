d6c1366aa05a5c347e0859a600b5971f48794593
yosef langer <yosef@doar.systems>
2023-02-23 10:00:09 +0200

    Update create-user.sh

--- create-user.sh
+++ create-user.sh
@@ -1,9 +1,9 @@
-USER=1$
+USER=$1
 OVPN_DATA="ovpn-data"
 
 docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full $USER nopass
 
-docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_otp_user $USER
+docker run -v $OVPN_DATA:/etc/openvpn --rm -it kylemanna/openvpn ovpn_otp_user $USER nopass
 
 docker run -v $OVPN_DATA:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient $USER > $USER.ovpn
 
